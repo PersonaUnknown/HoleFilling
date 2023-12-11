4b5beccdac8e5f6ac07132ab0f9432bb87b482d9
awzhao1 <awzhao@wustl.edu>
2023-12-10 20:20:17 -0600

    finished refinement

--- WebApp/js/project/refine.js
+++ WebApp/js/project/refine.js
@@ -2,19 +2,25 @@ function position(expr, pattern) {
     var cases = []
     for (let i = 0; i < expr.length; i++) {
         var element = expr[i];
-        if (!Array.isArray(pattern)) {
-            if(Array.isArray(element)) {
-                for(let j = 0; j < element.length; j++) {
-                    var el = element[j];
-                    if(equal(el, pattern)) {
-                        cases.push([i])
+        if(Array.isArray(element)) {
+            for (let j=0; j< element.length; j++) {
+                var elem = element[j];
+                if(Array.isArray(elem)) {
+                    for(let k=0; k<element.length; k++) {
+                        var el = elem[k]
+                        if(equal(el, pattern)) {
+                            cases.push([i,j,k])
+                        }
                     }
                 }
+                if(equal(elem, pattern)) {
+                    cases.push([i,j])
+                }
             }
-            else if (equal(element, pattern)) { cases.push([i])}
-        } else {
-            if (element== pattern) {cases.push([i])}
-        }   
+        }
+        if(equal(element,pattern)) {
+            cases.push([i])
+        }
     }
     return cases;
 }
@@ -23,8 +29,8 @@ function position(expr, pattern) {
 function refine(mesh, patches) {
     var surface = mesh.getMeshData();
     var area = triangulate(mesh, "AREA");
-    var verts = area[0][0];
-    var faces = area[0][1];
+    var verts = surface[0];
+    var faces = surface[1];
     var holes = mesh.getHoles();
     var adjList = mesh.getAdjList();
     var removeTri, pos, pos2, positions = [];
@@ -37,67 +43,135 @@ function refine(mesh, patches) {
         var n = hole.length;
         var adjEdges = getAllEdges(surface, hole, adjList);
         var scale = new Object();
-
+        
         // For each vertex on hole boundary, compute average length
-        for (let j = 0; j < n; j++) {
-            var x = adjEdges[hole[j]];
-            scale[hole[j]] = math.mean(x);
+        for (let j = 0; j < verts.length; j++) {
+            
+            var x = adjEdges[verts[j]];
+            if(!(typeof x === 'undefined')) {
+                for (let k = 0; k < x.length; k++) {
+                    if(x[k]!=[])
+                        scale[j] = math.mean(x);
+                }
+            }
         }
-
         while (changed) {
             changed = false;
             // For every triangle in the patching mesh
             var length = patch.length;
 
-            for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length; j++) {
                 
-                var tri = patch[i];
+                var tri = patch[j];
                 var I = tri[0]; var J = tri[1]; var K = tri[2];
                 var p1 = verts[I]; var p2 = verts[J]; var p3 = verts[K];
-
                 // Compute centroid and scale
                 var centroid =  [(p1[0] + p2[0] + p3[0]) / 3, (p1[1] + p2[1] + p3[1]) / 3, (p1[2] + p2[2] + p3[2]) / 3];
-                var triScale = (scale[verts[adjList[p1].index]] + scale[verts[adjList[p2].index]] + scale[verts[adjList[p3].index]]) / 3;
-                
+                var triScale = (scale[I] + scale[J] + scale[K]) / 3;
+                var replace = true
                 // For m = i, j, k
-                if (alpha * math.norm(math.subtract(centroid, p1)) > triScale && alpha * math.norm(math.subtract(centroid, p1)) > scale[p1]) {
-                    // Add centroid as point
-                    verts.push(centroid)
-                    var c = verts.length - 1;
-                    patch[i] = [c, J, K];
-
-                    // Add two triangles
-                    patch.push([I, c, K])
-                    patch.push([I, J, c])
-                } else if (alpha * math.norm(math.subtract(centroid, p2)) > triScale && alpha * math.norm(math.subtract(centroid, p2)) > scale[p2]) {
-                    // Add centroid as point
-                    verts.push(centroid)
-                    var c = verts.length;
-                    patch[i] = [c, J, K];
-
-                    // Add two triangles
-                    patch.push([I, c, K])
-                    patch.push([I, J, c])
-                } else if (alpha * math.norm(math.subtract(centroid, p3)) > triScale && alpha * math.norm(math.subtract(centroid, p3)) > scale[p3]) {
-                    // Add centroid as point
-                    verts.push(centroid)
-                    var c = verts.length;
-                    patch[i] = [c, J, K];
-
-                    // Add two triangles
-                    patch.push([I, c, K])
-                    patch.push([I, J, c])
+                if (alpha * math.norm(math.subtract(centroid, p1)) <= triScale || alpha * math.norm(math.subtract(centroid, p1)) <= scale[I]) {
+                    // check scale
+                    replace = false;
+                    
+                } else if (alpha * math.norm(math.subtract(centroid, p2)) <= triScale || alpha * math.norm(math.subtract(centroid, p2)) <= scale[J]) {
+                    
+                    replace = false;
+                } else if (alpha * math.norm(math.subtract(centroid, p3)) <= triScale || alpha * math.norm(math.subtract(centroid, p3)) <= scale[K]) {
+                    
+                    replace = false;
                 }
+                if (replace) {
+                    
+                    verts.push(centroid)
+                    let c = verts.length - 1;
+                    patch[j] = [c, J, K];
+
+                    // Add two triangles
+                    patch.push([I, c, K])
+                    patch.push([I, J, c])
+                    
+                    // relax edges, relax edge I, J
+                    let positions = position(patch, I)
+                    let pos2 = []
+                    let posTracker = []
+                    for(let k = 0; k< positions.length;k++) {
+                        pos2.push(patch[positions[k][0]])
+                        posTracker.push(positions[k][0])
+                    }
+                    let pos = position(pos2, J);
+                    let changePos = 0;
+                    let T = -1;
+                    for(let k =0; k< pos.length; k++) {
+                        let temp = pos2[pos[k][0]][3 - position(pos2[pos[k][0]],I)[0]-pos[k][1]];
+                        
+                        if(temp!=K && temp != c) {
+                            T = temp;
+                            changePos = posTracker[pos[k][0]];
+                        }
+                    }
+                    if(T!=-1 && math.distance(verts[I], verts[J]) > math.distance(centroid,verts[T])){
+                        patch[changePos][3 - position(patch[changePos],I)[0] - position(patch[changePos], T)[0]] = c;
+                        patch[patch.length-1][2]=T;
+                    }
+
+                    //relax edge I,K
+                    pos = position(pos2, K);
+                    changePos = 0;
+                    T = -1;
+                    for(let k =0; k< pos.length; k++) {
+                        let temp = pos2[pos[k][0]][3 - position(pos2[pos[k][0]],I)[0]-pos[k][1]];
+                        
+                        if(temp!=J && temp != c) {
+                            T = temp;
+                            changePos = posTracker[pos[k][0]];
+                        }
+                    }
+                    if(T!=-1 && math.distance(verts[I], verts[K]) > math.distance(centroid,verts[T])){
+                        patch[changePos][3 - position(patch[changePos],I)[0] - position(patch[changePos], T)[0]] = c;
+                        patch[patch.length-2][0]=T;
+                    }
+
+                    //relax edge J,K
+                    positions = position(patch, K)
+                    pos2 = []
+                    posTracker = []
+                    for(let k = 0; k< positions.length;k++) {
+                        pos2.push(patch[positions[k][0]])
+                        posTracker.push(positions[k][0])
+                    }
+                    pos = position(pos2, J);
+                    changePos = 0;
+                    T = -1;
+                    for(let k =0; k< pos.length; k++) {
+                        let temp = pos2[pos[k][0]][3 - position(pos2[pos[k][0]],K)[0]-pos[k][1]];
+                        
+                        if(temp!=I && temp != c) {
+                            T = temp;
+                            changePos = posTracker[pos[k][0]];
+                        }
+                    }
+                    if(T!=-1 && math.distance(verts[J], verts[K]) > math.distance(centroid,verts[T])){
+                        patch[changePos][3 - position(patch[changePos],J)[0] - position(patch[changePos], T)[0]] = c;
+                        patch[j][2]=T;
+                    }
+                }
+    
             }
         }
     }
-
+    
     // Combine new patches with existing triangles
-    var newFaces = faces.concat(patches.flat());
+    
+    var newFaces = faces.concat(patch);
+    
     return [verts, newFaces];
 }
 
 function equal(a1, a2) {
+    if(!Array.isArray(a1)) {
+        return a1 == a2;
+    }
     if (a1.length != a2.length) {
         return false;
     }
